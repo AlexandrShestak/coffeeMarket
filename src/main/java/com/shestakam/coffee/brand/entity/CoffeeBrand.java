0e7c4ecdc5e880cd14de1ac07cdd8d0602b96836
AlexandrShestak <shestakalexandr@mail.ru>
2015-09-08 10:06:58 +0300

    add CoffeeBrand class

---
+++ src/main/java/com/shestakam/coffee/brand/entity/CoffeeBrand.java
@@ -0,0 +1,35 @@
+package com.shestakam.coffee.brand.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by shestakam on 8.9.15.
+ */
+@Entity
+@Table(name = "coffee_brand" ,catalog = "clientHelper")
+public class CoffeeBrand {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name",unique = true, nullable = false, length = 20)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/shestakam/coffee/controller/CoffeeController.java
@@ -0,0 +1,7 @@
+package com.shestakam.controller;
+
+/**
+ * Created by shestakam on 1.9.15.
+ */
+public class CoffeeController {
+}
