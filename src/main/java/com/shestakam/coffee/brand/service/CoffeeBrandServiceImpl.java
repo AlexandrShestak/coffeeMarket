0c9faa904faafcc285a0a26b9484f631b42477c6
AlexandrShestak <shestakalexandr@mail.ru>
2015-09-08 11:34:46 +0300

    add log4j logger to CoffeeBrandServiceImpl

--- src/main/java/com/shestakam/coffee/brand/service/CoffeeBrandServiceImpl.java
+++ src/main/java/com/shestakam/coffee/brand/service/CoffeeBrandServiceImpl.java
@@ -2,6 +2,8 @@ package com.shestakam.coffee.brand.service;
 
 import com.shestakam.coffee.brand.dao.CoffeeBrandDao;
 import com.shestakam.coffee.brand.entity.CoffeeBrand;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.List;
 @Service
 public class CoffeeBrandServiceImpl implements CoffeeBrandService {
 
+    private final static Logger logger = LogManager.getLogger(CoffeeBrandServiceImpl.class);
+
     private CoffeeBrandDao coffeeBrandDao;
 
     @Autowired
@@ -21,22 +25,27 @@ public class CoffeeBrandServiceImpl implements CoffeeBrandService {
     }
 
     public Long save(CoffeeBrand coffeeBrand) {
+        logger.debug("add coffee brand");
         return coffeeBrandDao.save(coffeeBrand);
     }
 
     public CoffeeBrand get(Long id) {
+        logger.debug("get coffee brand with id : "+id);
         return coffeeBrandDao.get(id);
     }
 
     public List<CoffeeBrand> getAll() {
+        logger.debug("get all coffee brands");
         return coffeeBrandDao.getAll();
     }
 
     public void delete(Long id) {
+        logger.debug("delete coffee brand with id : "+id);
         coffeeBrandDao.delete(id);
     }
 
     public void update(CoffeeBrand coffeeBrand) {
+        logger.debug("update coffee brand with id : " +coffeeBrand.getId());
         coffeeBrandDao.update(coffeeBrand);
     }
 }
