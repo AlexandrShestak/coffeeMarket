1c98342ecd31881080ff8b97c5064fd3a36f02b8
AlexandrShestak <shestakalexandr@mail.ru>
2015-09-10 17:15:09 +0300

    add brands to jsp from database by angular

--- src/main/java/com/shestakam/coffee/controller/CoffeeController.java
+++ src/main/java/com/shestakam/coffee/controller/CoffeeController.java
@@ -1,10 +1,18 @@
 package com.shestakam.coffee.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by shestakam on 1.9.15.
  */
-@RestController
+@Controller
 public class CoffeeController {
+
+    @RequestMapping(value = "/coffee")
+    public String returnPage(){
+        return "coffee";
+    }
+
 }
--- src/main/webapp/script/brandController.js
+++ src/main/webapp/script/brandController.js
@@ -1,13 +1,59 @@
-angular.module('todoApp', [])
-    .controller('SimpleBrandController', function() {
-        var brandList = this;
-        brandList.brands = [
-            {name:'Arabica'},
-            {name:'Robusta'},
-            {name:'Folgers'},
-            {name:'Nescafe'},
-            {name:'Gevalia'}
-        ];
+var myApp = angular.module('todoApp', [ 'ngRoute', 'ngResource' ]);
+
+myApp.config(['$resourceProvider', function($resourceProvider) {
+    // Don't strip trailing slashes from calculated URLs
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
+
+myApp.factory('brandFactory',function($resource){
+    return $resource('/brands/:id');
+   /* return {
+        async: function () {
+            return $http.get('brands.json');  //1. this returns promise
+        }
+    }
+   /!* var factory={};
+    var brandList;
+     $http.get('/brands').success(function(response){
+     brandList = response;
+     })*!/
+     var brandList = [
+     {name:'Arabica'},
+     {name:'Robusta'},
+     {name:'Folgers'},
+     {name:'Nescafe'},
+     {name:'Gevalia'},
+     {name:'Gevalia1111'}
+     ];
+
+    factory.getBrands = function(){
+        $http.get('/brands').then(function(response){
+            var  brandList = response.data;
+            alert('2')
+            alert(brandList[1].name);
+        })
+        $scope.brands = brandList
+        return brandList;
+    };
+    return factory;*/
+});
+
+myApp.controller('SimpleBrandController', function($scope,brandFactory) {
+    $scope.brands = brandFactory.query(function() {
+        console.log("tratata");
     });
+   /* alert('1')
+    brandFactory.async().then(function(d) { //2. so you can use .then()
+        $scope.brands = d;
+    });
+    $scope.brands = brandFactory.getBrands();*/
+});
 
 
+
+/*myApp.factory('Phone', ['$resource',
+    function($resource){
+        return $resource('phones/.json', {}, {
+            query: {method:'GET', params:{}, isArray:true}
+        });
+    }]);*/
