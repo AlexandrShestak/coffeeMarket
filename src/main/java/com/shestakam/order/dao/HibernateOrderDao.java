24286ee4d2a9ad0a6f533d13498ec4afab1a91df
AlexandrShestak <shestakalexandr@mail.ru>
2015-09-22 23:09:32 +0300

    add hibermate order dao

---
+++ src/main/java/com/shestakam/order/dao/HibernateOrderDao.java
@@ -0,0 +1,65 @@
+package com.shestakam.order.dao;
+
+import com.shestakam.order.entity.Order;
+import com.shestakam.order.orderItem.entity.OrderItem;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by shestakam on 22.9.15.
+ */
+public class HibernateOrderDao implements OrderDao {
+
+    private final static Logger logger = LogManager.getLogger(HibernateOrderDao.class);
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+
+    @Override
+    public Long save(Order order) {
+        logger.debug("add order");
+        Session session = sessionFactory.getCurrentSession();
+        session.save(order);
+        return order.getId();
+    }
+
+    @Override
+    public Order get(Long id) {
+        logger.debug("get order with id : "+id);
+        Session session = sessionFactory.getCurrentSession();
+        Order order = (Order) session.get(Order.class, id);
+        return order;
+    }
+
+    @Override
+    public List<Order> getAll() {
+        logger.debug("get all orders");
+        Session session = sessionFactory.getCurrentSession();
+        List result = session.createQuery("from com.shestakam.order.entity.Order").list();
+        return result;
+    }
+
+    @Override
+    public void delete(Long id) {
+        logger.debug("delete order with id : "+id);
+        Session session = sessionFactory.getCurrentSession();
+        Order order = (Order) session.get(Order.class, id);
+        session.delete(order);
+    }
+
+    @Override
+    public void update(Order order) {
+        logger.debug("update order  with id : " + order.getId());
+        Session session = sessionFactory.getCurrentSession();
+        session.update(order);
+    }
+}
---
+++ src/main/java/com/shestakam/order/dao/OrderDao.java
@@ -0,0 +1,10 @@
+package com.shestakam.order.dao;
+
+import com.shestakam.db.GenericDao;
+import com.shestakam.order.entity.Order;
+
+/**
+ * Created by shestakam on 22.9.15.
+ */
+public interface OrderDao extends GenericDao<Order> {
+}
